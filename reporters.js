de8a150f80d941ab5d0c51370e6f76a404929787
lll69 <60803753+lll69@users.noreply.github.com>
2022-05-16 09:37:04 +0800

    Update reporters.js

--- reporters.js
+++ reporters.js
@@ -5,6 +5,20 @@ class Reporters {
             name: 'Reporters',
             blocks: [
                 {
+                    opcode: 'strictlyEquals',
+                    blockType: Scratch.BlockType.BOOLEAN,
+                    text: '[ONE] === [TWO]',
+                    arguments: {
+                        ONE: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'Value'
+                        },
+                        TWO: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'value'
+                        }
+                    }
+                }, {
                     opcode: 'True',
                     blockType: Scratch.BlockType.BOOLEAN,
                     text: 'true'
@@ -16,14 +30,34 @@ class Reporters {
                     opcode: 'nan',
                     blockType: Scratch.BlockType.REPORTER,
                     text: 'NaN'
-                },{
+                }, {
                     opcode: 'infinity',
                     blockType: Scratch.BlockType.REPORTER,
                     text: 'Infinity'
-                },{
+                }, {
                     opcode: 'obj',
                     blockType: Scratch.BlockType.REPORTER,
                     text: '{}'
+                }, {
+                    opcode: 'array',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: '[]'
+                }, {
+                    opcode: 'getLocation',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'location'
+                },{
+                    opcode: 'referrer',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'referrer'
+                }, {
+                    opcode: 'userAgent',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'userAgent'
+                }, {
+                    opcode: 'language',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'language'
                 }
             ]
         };
@@ -40,8 +74,27 @@ class Reporters {
     infinity() {
         return Infinity;
     }
-    obj(){
+    obj() {
         return {};
+    }
+    array() {
+        return [];
+    }
+    strictlyEquals(args) {
+        // Note strict equality: Inputs must match exactly: in type, case, etc.
+        return args.ONE === args.TWO;
+    }
+    getLocation() {
+        return location.toString();
+    }
+    referrer(){
+        return document.referrer;
+    }
+    userAgent() {
+        return navigator.userAgent;
+    }
+    language() {
+        return navigator.language;
     }
 }
 Scratch.extensions.register(new Reporters());
