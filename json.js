a6251b41d8d03eff55cda7f6636d580d76241092
lll69 <60803753+lll69@users.noreply.github.com>
2022-08-22 11:10:40 +0800

    Update

--- json.js
+++ json.js
@@ -3,71 +3,71 @@ class Json {
         return {
             id: 'json',
             name: 'JSON',
-            blocks: [ {
-                    opcode: 'LENGTH',
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: 'length of [OBJ]',
-                    arguments: {
-                        OBJ: {
-                            type: Scratch.ArgumentType.STRING,
-                            defaultValue: 'Object'
-                        }
-                    }
-                },  {
-                    opcode: 'OF',
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: 'item [INDEX] of [OBJ]',
-                    arguments: {
-                        INDEX: {
-                            type: Scratch.ArgumentType.STRING,
-                            defaultValue: '0'
-                        },
-                        OBJ: {
-                            type: Scratch.ArgumentType.STRING,
-                            defaultValue: 'Object'
-                        }
-                    }
-                },{
-                    opcode: 'stringify',
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: 'stringify [OBJ]',
-                    arguments: {
-                        OBJ: {
-                            type: Scratch.ArgumentType.STRING,
-                            defaultValue: 'Object'
-                        }
-                    }
-                },{
-                    opcode: 'tostring',
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: 'toString [OBJ]',
-                    arguments: {
-                        OBJ: {
-                            type: Scratch.ArgumentType.STRING,
-                            defaultValue: 'Object'
-                        }
-                    }
-                }, {
-                    opcode: 'parseJSON',
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: 'parseJSON [STR]',
-                    arguments: {
-                        STR: {
-                            type: Scratch.ArgumentType.STRING,
-                            defaultValue: '{}'
-                        }
-                    }
-                }, {
-                    opcode: 'TYPEOF',
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: 'typeof [OBJ]',
-                    arguments: {
-                        OBJ: {
-                            type: Scratch.ArgumentType.STRING,
-                            defaultValue: ''
-                        }
+            blocks: [{
+                opcode: 'OF',
+                blockType: Scratch.BlockType.REPORTER,
+                text: 'item [INDEX] of [OBJ]',
+                arguments: {
+                    INDEX: {
+                        type: Scratch.ArgumentType.STRING,
+                        defaultValue: '0'
+                    },
+                    OBJ: {
+                        type: Scratch.ArgumentType.STRING,
+                        defaultValue: '{}'
                     }
                 }
+            }, {
+                opcode: 'LENGTH',
+                blockType: Scratch.BlockType.REPORTER,
+                text: 'length of [OBJ]',
+                arguments: {
+                    OBJ: {
+                        type: Scratch.ArgumentType.STRING,
+                        defaultValue: 'Object'
+                    }
+                }
+            }, {
+                opcode: 'stringify',
+                blockType: Scratch.BlockType.REPORTER,
+                text: 'stringify [OBJ]',
+                arguments: {
+                    OBJ: {
+                        type: Scratch.ArgumentType.STRING,
+                        defaultValue: 'Object'
+                    }
+                }
+            }, {
+                opcode: 'tostring',
+                blockType: Scratch.BlockType.REPORTER,
+                text: 'toString [OBJ]',
+                arguments: {
+                    OBJ: {
+                        type: Scratch.ArgumentType.STRING,
+                        defaultValue: 'Object'
+                    }
+                }
+            }, {
+                opcode: 'parseJSON',
+                blockType: Scratch.BlockType.REPORTER,
+                text: 'parseJSON [STR]',
+                arguments: {
+                    STR: {
+                        type: Scratch.ArgumentType.STRING,
+                        defaultValue: '{}'
+                    }
+                }
+            }, {
+                opcode: 'TYPEOF',
+                blockType: Scratch.BlockType.REPORTER,
+                text: 'typeof [OBJ]',
+                arguments: {
+                    OBJ: {
+                        type: Scratch.ArgumentType.STRING,
+                        defaultValue: ''
+                    }
+                }
+            }
             ]
         };
     }
@@ -75,7 +75,7 @@ class Json {
         return args.OBJ.length;
     }
     OF(args) {
-        return args.OBJ[args.INDEX];
+        return JSON.stringify(JSON.parse(args.OBJ)[args.INDEX]);
     }
     stringify(args) {
         return JSON.stringify(args.OBJ);
@@ -83,11 +83,11 @@ class Json {
     tostring(args) {
         return String(args.OBJ);
     }
-    parseJSON(args){
+    parseJSON(args) {
         return JSON.parse(args.STR);
     }
-    TYPEOF(args){
-        return typeof(args.OBJ);
+    TYPEOF(args) {
+        return typeof (args.OBJ);
     }
 }
 Scratch.extensions.register(new Json());
